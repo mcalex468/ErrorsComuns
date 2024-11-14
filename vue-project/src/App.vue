0467d5f108dcd80429be88f98b80e0270946ab63
mcalex468 <alexmaco468@gmail.com>
2024-11-14 18:19:40 +0100

    c

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import KeyForSample from './components/KeyForSample.vue';
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
@@ -8,12 +9,11 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <KeyForSample />
   </main>
 </template>
 
